fa6fef1081aa7a6f5e0c82f63424dc6a53c57a4c
yanheven <yanheven@gmail.com>
2014-12-19 22:16:56 +0800

    add shell

--- devstack.sh
+++ devstack.sh
@@ -1,4 +1,9 @@
+adduser stack
+apt-get install sudo -y
+echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+su stack
 cd ~
 git clone https://code.csdn.net/openstack-dev/devstack.git
 sed -i "s/{GIT_BASE:-git:\/\/git.openstack.org}/{GIT_BASE:-https:\/\/code.csdn.net\/openstack}/g" ~/devstack/stackrc
-
+cd devstack
+./stack.sh
