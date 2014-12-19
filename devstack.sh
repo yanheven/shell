ed1346c1102f409830ded1085f65dc2c8e9ae6d0
yanheven <yanheven@gmail.com>
2014-12-19 22:00:40 +0800

    add shell

--- devstack.sh
+++ devstack.sh
@@ -1,5 +1,5 @@
 cd ~
-git clone https://code.csdn.net/openstack-dev/devstack
+git clone https://code.csdn.net/openstack-dev/devstack.git
 cd devstack
 sed -i "s/{GIT_BASE:-git:\/\/git.openstack.org}/{GIT_BASE:-https:\/\/code.csdn.net\/openstack}/g"
 
