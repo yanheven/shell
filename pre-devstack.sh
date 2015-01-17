f4594b3cb667bcf983bfcdaadcdb95b028418ad9
yanheven <yanheven@gmail.com>
2015-01-17 23:47:55 +0800

    add pre-devstack for previous installation of devstack

---
+++ pre-devstack.sh
@@ -0,0 +1,7 @@
+mkdir /opt/stack
+REMOTE_HOST=58.67.194.157
+PROJECT=("keystone" "glance" "cinder" "nova" "horizon" "heat" "requirements")
+for pro in "${PROJECT[@]}";do scp root@$REMOTE_HOST:/opt/stack/$pro
+/opt/stack/;chown -R stack:stack /opt/stack/$pro;done
+sed -i "s/-git://git.openstack.org/github.com/" /home/stack/devstack/stackrc
+
