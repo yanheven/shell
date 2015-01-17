05ff38e146f9fa885c46050492b1efac51afdacf
yanheven <yanheven@gmail.com>
2015-01-18 00:21:48 +0800

    d

--- to-240.sh
+++ to-240.sh
@@ -1,5 +1,4 @@
-DIR=("cinder" "glance" "heat" "heat-cfntools" "heat-templates" "horizon"
-"keystone" "nova" "requirements" "tempest")
+DIR=("cinder" "glance" "heat" "heat-cfntools" "heat-templates" "horizon" "keystone" "nova" "requirements" "tempest")
 for dir in "${DIR[@]}";do scp -r /opt/stack/$die 192.168.2.240:/root/stack/$dir
 scp -r /home/stack/devstack/ 192.168.2.240:/root/stack/
 
