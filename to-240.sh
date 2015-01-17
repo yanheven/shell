42ed75eb51ab29e9b87969dce17bda3bbf81a2bd
yanheven <yanheven@gmail.com>
2015-01-18 00:18:37 +0800

    a

--- to-240.sh
+++ to-240.sh
@@ -1,3 +1,5 @@
-scp -r /opt/stack 192.168.2.240:/root/
-scp -r /home/stack/devstack/ 192.168.2.240:/root/
+DIR=("cinder" "glance" "heat" "heat-cfntools" "heat-templates" "horizon"
+"keystone" "nova" "requirements" "tempest")
+for dir in "${DIR[@]}";do scp -r /opt/stack/$die 192.168.2.240:/root/stack/$dir
+scp -r /home/stack/devstack/ 192.168.2.240:/root/stack/
 
