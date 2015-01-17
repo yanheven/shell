185d3c42f5c04ecacc0a13dd07c419d3fb142577
yanheven <yanheven@gmail.com>
2015-01-18 01:12:29 +0800

    d

--- to-240.sh
+++ to-240.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-STACK_DIR=("cinder" "glance" "heat" "heat-cfntools" "heat-templates")
-STACK_DIR2=("horizon" "keystone" "nova" "requirements" "tempest")
+#!/bin/bash
+STACK_DIR=("cinder" "glance" "heat" "heat-cfntools" "heat-templates" "horizon" "keystone" "nova" "requirements" "tempest")
 for stack_dir in "${STACK_DIR[@]}";do scp -r /opt/stack/$stack_dir 192.168.2.240:/root/stack/$stack_dir;done
-for stack_dir in "${STACK_DIR2[@]}";do scp -r /opt/stack/$stack_dir 192.168.2.240:/root/stack/$stack_dir;done
-scp -r /home/stack/devstack/ 192.168.2.240:/root/stack/
+#scp -r /home/stack/devstack/ 192.168.2.240:/root/stack/
 
